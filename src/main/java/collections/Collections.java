4b5330dddd1c471e76b7676f47624be24a27b679
rafasilvadev <rafabdasilva12@gmail.com>
2022-10-06 16:04:13 -0300

    iniciando projeto

---
+++ src/main/java/collections/Collections.java
@@ -0,0 +1,12 @@
+package collections;
+
+import org.apache.commons.collections.BidiMap;
+import org.apache.commons.collections.bidimap.TreeBidiMap;
+
+public class Collections {
+    //Lista Ligada - LinkedMap();
+    //Arvore - TreeBidiMap();
+    //HashedMap - Tipo JSON
+    //PriorityQueue - tem o PriorityQueue e o seu substituto Buffer
+    BidiMap bidi = new TreeBidiMap();
+}
