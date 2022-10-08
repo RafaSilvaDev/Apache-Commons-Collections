4f085673241684bdb1d4dfefeec3fb0eee67689c
rafasilvadev <rafabdasilva12@gmail.com>
2022-10-08 18:19:31 -0300

    Tentando...

--- src/main/java/collections/Collections.java
+++ src/main/java/collections/Collections.java
@@ -1,12 +1,47 @@
 package collections;
 
-import org.apache.commons.collections.BidiMap;
-import org.apache.commons.collections.bidimap.TreeBidiMap;
+
+import model.Produto;
+import org.apache.commons.collections4.SetUtils;
+
+import java.util.*;
 
 public class Collections {
-    //Lista Ligada - LinkedMap();
-    //Arvore - TreeBidiMap();
-    //HashedMap - Tipo JSON
-    //PriorityQueue - tem o PriorityQueue e o seu substituto Buffer
-    BidiMap bidi = new TreeBidiMap();
+
+    public static void main(String[] args) {
+        //HashSet is unsorted and unordered
+
+        //LinkedHashSet is ordered and sorted
+
+        //TreeSet is sorted
+        Produto produto1 = new Produto("123", "Laptop Lenovo", 2000.0, 10);
+        Produto produto2 = new Produto("321", "Picanha 1kg", 40.0, 10);
+        Produto produto3 = new Produto("879", "Teclado Razer", 1000.0, 0);
+        Produto produto4 = new Produto("012", "Samsung galaxy S7 64Gb", 2250.5, 0);
+        // não aceitam duplicatas
+        Produto produto5 = new Produto("012", "Xiaomi Mi 9", 2500.5, 0);
+
+        Set<Produto> produtoSet = new HashSet<>();
+        produtoSet.add(produto1);
+        produtoSet.add(produto2);
+        produtoSet.add(produto3);
+        produtoSet.add(produto4);
+        produtoSet.add(produto5);
+
+        System.out.println("# # # HashSet # # #");
+        for(Produto p : produtoSet){
+            System.out.println(p);
+        }
+
+        Set<Produto> newProdutoSet;
+        newProdutoSet = SetUtils.orderedSet(produtoSet);
+
+
+        System.out.println("\n# # # HashSet Apache # # #");
+        for(Produto p : newProdutoSet){
+            System.out.println(p);
+        }
+
+
+    }
 }
---
+++ src/main/java/model/Atleta.java
@@ -0,0 +1,27 @@
+package model;
+
+public class Atleta {
+    private int numero;
+    private String nome;
+
+    public Atleta(int numero, String nome){
+        this.numero = numero;
+        this.nome = nome;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
